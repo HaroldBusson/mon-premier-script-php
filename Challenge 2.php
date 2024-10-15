c3e1b831e76c131074fcdbed72898fe241ad4fe9
Harold <harolddev75@gmail.com>
2024-10-15 15:05:34 +0200

    first commit

---
+++ Challenge 2.php
@@ -0,0 +1,27 @@
+<?php
+$message1="0@sn9sirppa@#?ia’jgtvryko1";
+$message2="q8e?wsellecif@#?sel@#?setuotpazdsy0*b9+mw@x1vj";
+$message3="aopi?sgnirts@#?sedhtg+p9l!";
+
+$message1="0@sn9sirppa@#?ia’jgtvryko1";
+$lenghtMessage1=mb_strlen($message1);
+$halfMessage1=$lenghtmessage1/2;
+$subStringKeyFigure=mb_substr($Message1,5,$lenghtMessage1);
+$replaceChain=str_ireplace ("@#?",'',$subStringKeyFigure);
+$reverseChain=strrev($replaceChain);
+
+$message2="q8e?wsellecif@#?sel@#?setuotpazdsy0*b9+mw@x1vj"
+$lenghtMessage2=mb_strlen($message2);
+$halfMessage=$lenghtmessage2/2;
+$subStringKeyFigure=mb_substr($Message2,5,$lenghtMessage1);
+$replaceChain=str_ireplace ("@#?",'',$subStringKeyFigure);
+$reverseChain=strrev($replaceChain);
+
+$message3="aopi?sgnirts@#?sedhtg+p9l!";
+$lenghtMessage3=mb_strlen($message3);
+$halfMessage=$lenghtmessage3/2;
+$subStringKeyFigure=mb_substr($Message3,5,$lenghtMessage1);
+$replaceChain=str_ireplace ("@#?",'',$subStringKeyFigure);
+$reverseChain=strrev($replaceChain);
+
+?>
---
+++ stevenson.php
@@ -0,0 +1,7 @@
+<?php 
+
+echo $title="Voyage avec un âne dans les Cévennes"; <br/>
+echo $bookRead=true;<br/>
+echo $yearPublished=2017;<br/>
+echo $userPrice=6,20;<br/>
+?>
