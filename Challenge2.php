c8d01606968ac2e298cd53ead2846f7394d1fd26
Harold <harolddev75@gmail.com>
2024-10-16 15:15:35 +0200

    add comments

--- Challenge2.php
+++ Challenge2.php
@@ -13,4 +13,5 @@ $message1="0@sn9sirppa@#?ia’jgtvryko1";
 $message2="q8e?wsellecif@#?sel@#?setuotpazdsy0*b9+mw@x1vj";
 $message3="aopi?sgnirts@#?sedhtg+p9l!";
 
+
 ?>  
--- Challenge3.php
+++ Challenge3.php
@@ -1,10 +1,12 @@
 <?php
-$book["L'Île_au_trésor"]=>[1883];
-$book["Saint_Yves"]=>[1897];
-$book ["L'Étrange_Cas_du_docteur_Jekyll_et_de_M.Hyde"]=>[1886];
-asort($book);
+$book["L'Île_au_trésor"=>1883; // array with books as key and year as value
+"Saint_Yves"=>1897;
+"L'Étrange_Cas_du_docteur_Jekyll_et_de_M.Hyde"=>1886
+];
 
-foreach($book as $year) {
+asort($book); // classement dans ordre croissant
+
+foreach($book as $titke => $year) { // Pour chaque livre montrer l'anné de publication
 
     echo $key;
     echo $book;
