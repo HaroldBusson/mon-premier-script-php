1ef8edf76f173d43452cfa0464193dcf6f665716
Harold <harolddev75@gmail.com>
2024-10-16 16:23:39 +0200

    movie tab created

---
+++ Challlenge4.php
@@ -0,0 +1,5 @@
+$movies = [ 
+"L'île au trésors"=>[Robert Newton,Basil Sydney, Andrew Blackett],
+"Doctor Jekyll"=>[Robyn Cara, Jonathan Hyde, Lindsay Duncan],
+"La Flèche noire"=>[Louis Hayward, Paul Cavanagh, Rhys Williams]
+];
