6790d1423920acceb86e1332820bf242e1a7e132
Harold <harolddev75@gmail.com>
2024-10-17 09:38:25 +0200

    add two loops and comments

--- Challlenge4.php
+++ Challlenge4.php
@@ -1,5 +1,16 @@
+<?php 
 $movies = [ 
-"L'île au trésors"=>[Robert Newton,Basil Sydney, Andrew Blackett],
-"Doctor Jekyll"=>[Robyn Cara, Jonathan Hyde, Lindsay Duncan],
-"La Flèche noire"=>[Louis Hayward, Paul Cavanagh, Rhys Williams]
-];
+"L'île au trésors" => [Robert Newton,Basil Sydney, Andrew Blackett],
+"Doctor Jekyll" => [Robyn Cara, Jonathan Hyde, Lindsay Duncan],
+"La Flèche noire" => [Louis Hayward, Paul Cavanagh, Rhys Williams]
+]; // Array with movies and actors
+
+foreach($movies as $movie) { // for each array movie show first step (movie's title)
+echo “Dans le film $movie[0], ";
+
+foreach($movies as $movie)
+echo "les principaux acteurs sont : $movie[1],[2],[3]"; // for each array movie show each 
+
+}
+
+?>
