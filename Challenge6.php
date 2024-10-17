980440127d53f65910a84ec2193834a7acc83e29
Harold <harolddev75@gmail.com>
2024-10-17 14:36:41 +0200

    indentation

---
+++ Challenge6.php
@@ -0,0 +1,10 @@
+<?php
+function writeSecretSentence($param1, $param2) {// function writeSecretSentence have created with 2 parametre
+        $sentence = "$param1 . s'incline à . $param2"; // Recreate a secret sentence
+
+        return $sentence; // return (renvoie) the sentence 
+}
+
+echo writeSecretSentence(return);
+    
+?>
