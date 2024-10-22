d0ed2353e9806a70a21c0d5323ac2e5513fd0a5c
Harold <harolddev75@gmail.com>
2024-10-22 11:19:54 +0200

    fixed

--- calculette.php
+++ calculette.php
@@ -1,9 +1,10 @@
 <?php
-echo $result_add = $argv[1] + $argv[2] . "\n"; // Opérateur dans la commande avec ($argv) 
-echo $result_sous = $arg[1] - $argv[2] . "\n";
-echo $result_multi = $arg[1] * $argv[2] . "\n";
-echo $result_divid = $arg[1] / $argv[2] . "\n";
-echo $result_sous = $arg[1] ** $argv[2] . "\n";
-echo $result_sous = $arg[1] % $argv[2] . "\n";
+
+echo "Addition" . $argv[1] + $argv[2] . "\n"; // Opérateur dans la commande avec ($argv) 
+echo "soustraction . $arg[1] - $argv[2] . "\n";
+echo "Multiplication" . $arg[1] * $argv[2] . "\n";
+echo "Division" . $arg[1] / $argv[2] . "\n";
+echo "Expotentiel" . $arg[1] ** $argv[2] . "\n";
+echo "Modulo" . $arg[1] % $argv[2] . "\n";
 
 ?>
