47fe46765366349f38f3e946f971e39383b4dfe5
Harold <harolddev75@gmail.com>
2024-10-22 14:30:09 +0200

    add comments

---
+++ boucles while & for.php
@@ -0,0 +1,13 @@
+<?php
+
+$valeur1=1;
+while($i!=10){  // tant que $i est différend de 10 il diminue de 1
+    $i--;
+}
+echo "Resultat": . $valeur1 "\n";
+
+for($i=0;$i<10;$i++) {   // $i commence à zero, tant que i est <10, le pas est de 1
+    echo $valeur1 = $i;
+    echo $valeur1 = "\n";
+}
+?>
