92cd2bcf4b1bda00b53cad5fef57ad65ab5383db
Harold <harolddev75@gmail.com>
2024-11-07 09:54:09 +0100

    add comments

--- ecommerce.sql
+++ ecommerce.sql
@@ -10,10 +10,9 @@ familyname VARCHAR (100) NOT NULL
 CREATE TABLE IF NOT EXISTS Order (
 id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-`status` ENUM('Created', 'check', 'ship') NOT NULL, -- . ENUM est plus explicite et plus facile à gérer pour des opérations de lecture et d'écriture.
+`status` ENUM('Created', 'check', 'ship') NOT NULL, -- ENUM est plus explicite et plus facile à gérer pour des opérations de lecture et d'écriture.
 FOREIGN KEY (idCustomer) REFERENCES Customer (id),
 total DECIMAL (10,2) NOT NULL,  -- DECIMAL evite les problèmes de précision
-
 );
 
 CREATE TABLE IF NOT EXITS Product (
@@ -28,5 +27,5 @@ id_order INT NOT NULL,
 id_product INT NOT NULL,
 FOREIGN KEY (id_order) REFERENCES `Order` (id) INT (100),
 FOREIGN KEY (id_product) REFERENCES Product (id) (100),
-quantity INT NOT NULL CHECL (quantity >0 ) -- Pour gara,ntir que la quantité soit pas négative
+quantity INT NOT NULL CHECK (quantity > 0 ) -- Pour garantir que la quantité soit pas négative
 );
--- formulaireecommerce.php
+++ formulaireecommerce.php
@@ -11,19 +11,24 @@
     <form action="/traitementeco.php" action="POST"></form>
     <div>
         <label for="lastname Custumer">Nom :</label>
-        <input type="text" id="familyname" name="familyname">
+        <input type="text" id="familyname" name="familyname" required
+            pattern="^[a-zA-ZÀ-ÿ\s\-]+$"
+            title="Le nom doit contenir que des lettres, des espaces et des tirets">
     </div>
     <div>
         <label for="firstname" id="firstname" name="firstname"></label>
-        <input type="text" id="familyname" name="firstname">
+        <input type="text" id="familyname" name="firstname" required>
     </div>
     <div>
         <label for="nom du produit">Nom du produit à ajouter: </label>
-        <input type="name" id="name" name="name">
+        <input type="name" id="name" name="name" required>
     </div>
     <div>
-    <label for="prix du produit">prix du produit :</label>    
-    <input type ="prix du produit" id="price" name="price">
+        <label for="prix du produit">prix du produit :</label>
+        <input type="number" id="price" name="price" step="0.01" required>
+    </div>
+    <div>
+        <button type="submit">Ajouter le produit</button>
     </div>
 </body>
 
